2c23c06f2af8983e1fd33195cf5a3dfa8cf46953
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-10-09 23:22:19 -0700

    Comments

--- BrownianPy/CoinToss.py
+++ BrownianPy/CoinToss.py
@@ -15,21 +15,22 @@ class CoinToss():
     ---------------------------------------------------------------------------------------------------------------------------------"""
     
     def __init__(self):
-        
         self.zeta = dict({'H':1, 'T':-1})
         self.RandVariables = list()
         self.SampleSpace = list()
-    
-
+   
+    #  returns a {-1,1} valued sequence
     def RandVariable(self, ω):
         return [self.zeta[ω[i]] for i in range(len(list(ω)))]
     
+    #  Add individual samples ω having length = Length to Ω 
     def addSample(self, Length: int):
         fArray = Maps.randFloats(Length)
         ω = [Maps.float2HT(fArray[i]) for i in range(Length)]
         self.SampleSpace.append(ω)
         self.RandVariables.append(self.RandVariable(ω))
-        
+    
+    # Add samples of length sizes = [a,b,c,...] to the Sample space  Ω 
     def addSamples(self, sizes):
         events = [self.addSample(size) for size in sizes]
         for event in events:
