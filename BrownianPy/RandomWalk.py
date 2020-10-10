997fab2c82535e31779d268375ce80e74c1a158e
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-10-09 23:14:31 -0700

    Refine RandomWalk Object

--- BrownianPy/RandomWalk.py
+++ BrownianPy/RandomWalk.py
@@ -1,121 +1,71 @@
 import numpy as np
+from CoinToss import CoinToss
 
-class RandomWalk():
-    def __init__(self, maxSteps = 100, fromCoinToss = False):
-        self.maxSteps = maxSteps
-        self.possibleSteps = [-1, 1]
+"""-------------------------------------------Inherited Variables--------------------------------------------------------------------
+
+    Sample Space:                            Ω = {ωₖ | for k=1,...,∞} = self.SampleSpace                                                        
+    Samples :                                    ω = (T,H,H,T,...) = elements of Ω             
+    
+    IID Random Variable :                ζ : {H, T} -> {1,-1}  = self.zeta
+    
+    Random Variable :                      X: ω-> {sequences of {-1,1}}  (elements of self.RandVariables)
+                                                                       X(ω) = (ζ₁ , ζ₂ , ζ ₃ ,...)  = (-1, 1, -1, ...)   
+-------------------------------------------------------------------------------------------------------------------------------------"""
+
+ """---------------------------------------RandomWalk Object Variables----------------------------------------------------------
+
+    States:                                         S = ( ζ ₁ , ζ ₁ +  ζ ₂ , ζ ₁ +  ζ₂  + ζ ₃ ,...) = self.States 
+    
+    Moving Averages:                        Avgs = ( S1, (S1+S2)/2, (S1+S2+S3)/3 ,... ) = self.Avgs
+
+--------------------------------------------------------------------------------------------------------------------------------------"""
+
+#  Inherits from the CoinToss class
+class RandomWalk(CoinToss):
+    
+    def __init__(self):
+        super().__init__()
+        self.States = list()
+        self.Avgs = list()
         
 
-        if fromCoinToss == True:
-            self.CT= CoinToss(self.maxSteps)
-            self.steps = C.PlusMinusHT()
-
-        else:
-            self.CT= None
-            self.steps = []
-            for i in range(maxSteps):
-                self.steps.append(np.random.choice(self.possibleSteps))
-
-        self.States = []
-        Sum = 0
-        for i in range(maxSteps):
-            Sum += self.steps[i]
-            self.States.append(Sum)
-
-    def movingAvgState(self):
-        self.movingAvg = []
-        temp = 0
-        for i in range(self.maxSteps):
-            temp+=self.States[i]/(i+1)
-            self.movingAvg.append(temp)
-        return self.movingAvg
-        
-    # Simulate The Random Walk Object
-    def simulate(self):
-        from matplotlib.animation import FuncAnimation
-        import matplotlib.pyplot as plt
-
-        again = True
-        while again == True:
-        
-            showAvg = str(input('Show Moving Average? [Y/N]: ')).lower()
-            nPlotted = 0
+    #  **kwards should be a list consisting of the number of Steps for each Random Walk.
+    #        can be a single number. If nothing is entered, it will create a defauly sample of
+    #        length 250,
+    def populate(self, **kwargs):
+        if len(kwargs) != 0:
+            N = list(kwargs.values())
             
-            plt.style.use('dark_background')
-
-            xdata = []
-            ydata = []
-            avg = []
-            
-
-            fig, ax = plt.subplots(figsize=(12,10))
-            l, =ax.plot([],[], color='salmon', lw = 0.5, label = 'State')
-            ax.plot([0*i + np.mean(self.States) for i in range(self.maxSteps)], linestyle='-', lw = 0.3, color = 'white',
-                    label = 'Mean')
-
-            avgs = self.movingAvgState()
-            l1, = ax.plot([],[],color = 'yellow', lw = 0.5,  linestyle = '--', label = 'Moving Average')
-            
-            time_txt = ax.text(0.05, 0.95,'',
-                            horizontalalignment='left',
-                            verticalalignment='top',
-                            transform=ax.transAxes)
-            
-            avgvalue = ax.text(0.05, 0.90,'',
-                            horizontalalignment='left',
-                            verticalalignment='top',
-                            transform=ax.transAxes)
-            
-            value = ax.text(0.05, 0.85,'',
-                            horizontalalignment='left',
-                            verticalalignment='top',
-                            transform=ax.transAxes)
-            
-            def init():
-                ax.set_ylim(min(np.min(avgs),np.min(self.States)-1), max(np.max(avgs),np.max(self.States)+1))
-                ax.title.set_text('Random Walk Simulation')
-                l.set_data([],[])
-                if showAvg == 'y':
-                    l1.set_data([],[])
-                time_txt.set_text("time-step: 0")
-                value.set_text("Current State: %d" %self.States[0])
-                avgvalue.set_text("Current Average: %.2f" %avgs[0])
-                return l, time_txt, value, avgvalue, l1, 
-        
-            def animate(i):
-                xdata.append(i)
-                ydata.append(self.States[i])
-                ax.set_xlim(0,i+1)
-                if showAvg == 'y':
-                    avg.append(avgs[i])
-                    l1.set_data(xdata,avg)
-                l.set_data(xdata, ydata)
-                time_txt.set_text("Current Time-Step: %d"%i)
-                avgvalue.set_text("Current Average: %.2f" %avgs[i])
-                value.set_text("Current State: %d" %self.States[i])
-                return l, time_txt,value, avgvalue, l1, 
-
-            anim = FuncAnimation(fig, animate,
-                                       init_func=init,
-                               frames=self.maxSteps, interval=1, blit=True, repeat = False)
-            
-            plt.xlabel('Time-step (n)')
-            plt.ylabel('State S')
-            plt.legend()
-            plt.show()
-            print("\n\n")
-            response = str(input("Plot a new Random Walk? [Y/N]:  ")).lower()
-            
-            print("\n\n")
-            if response == 'n':
-                again = False
-                return
+            if len(kwargs.values()) == 1:
+                self.addSample(N[0])
+                
             else:
-                newN = int(input("Enter the max number of steps to take: "))
-                self.maxSteps = newN
-                self.States = []
-                Sum = 0
-                for i in range(newN):
-                    self.steps.append(np.random.choice(self.possibleSteps))
-                    Sum += self.steps[i]
-                    self.States.append(Sum)
+                self.addSamples(N)                
+        else:
+            self.addSample(250)
+
+   # Calculates the Random walk for all Samples
+    def calculateStates(self):
+        for var in self.RandVariables:
+            Sum = 0
+            State = []
+            
+            for k in var:
+                Sum += k
+                State.append(Sum)
+        self.States.append(State)
+
+
+    #  Calculates the Moving average for each set of States
+    def movingAvgState(self):
+        N = len(self.States)
+
+        for i in range(N):
+            Sum = 0
+            Avg = []
+            X = self.States[i]
+            
+            for j in range(len(X)):
+                Sum += X[j]
+                Avg.append(Sum/(j+1))
+            self.Avgs.append(Avg)
