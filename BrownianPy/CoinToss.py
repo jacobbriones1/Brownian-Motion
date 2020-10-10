82b0c18044112ce44864f8483b474efaa15ea3c0
Jacob Briones <70331998+jacobbriones1@users.noreply.github.com>
2020-10-09 23:16:46 -0700

    Update CoinToss.py

--- BrownianPy/CoinToss.py
+++ BrownianPy/CoinToss.py
@@ -1,36 +1,37 @@
 import numpy as np
-from collections import Counter
+import Maps
 
-# Helper Function: Maps [0,1] to {H,T}
-def float2HT(x):
-    if x < 0 or x > 1:
-        print('Input must be a random number in the range [0,1]')
-        return
-    elif 0 <= x and x <0.5:
-        return 'T'
-    else:
-        return 'H'
-
-class CoinToss:
-    def __init__(self, maxOutcomes = 10):
-        self.maxOutcomes = maxOutcomes
-        self.map = dict({'H':1, 'T':0})
-        self.inverseMap = dict({1:'H', 0:'T'})
-        self.zeta = None
+class CoinToss():
+    
+    """ --------------------------------------- Class Variables------------------------------------------------------------------------
+    
+    Sample Space:                            Ω = {ωₖ | for k=1,...,∞} = self.SampleSpace
+    Samples :                                ω = (T,H,H,T,...) = elements of Ω
+    
+    IID Random Variable :                    ζ : {H, T} -> {1,-1}  = self.zeta
+    
+    Random Variable :                        X: ω-> {sequences of {-1,1}}  (elements of self.RandVariables)
+                                                                       X(ω) = (ζ₁ , ζ₂ , ζ ₃ ,...)  = (-1, 1, -1, ...)   
+    ---------------------------------------------------------------------------------------------------------------------------------"""
+    
+    def __init__(self):
         
-        if maxOutcomes != None:
-            randNums = np.random.rand(maxOutcomes)
-            self.Outcomes= [float2HT(x) for x in randNums]
+        self.zeta = dict({'H':1, 'T':-1})
+        self.RandVariables = list()
+        self.SampleSpace = list()
+    
 
-    def setMaxOutcomes(N):
-        self.maxOutcomes = N
-        randNums = np.random.rand(N)
-        self.Outcomes= [float2HT(x) for x in randNums]
-
-    # Maps {H,T} to {-1,1}
-    def PlusMinusHT(self):
-        self.zeta = [self.map[X] for X in self.Outcomes]
-        return self.zeta
-
-    def frequency(self):
-        return dict(Counter(self.Outcomes))
+    def RandVariable(self, ω):
+        return [self.zeta[ω[i]] for i in range(len(list(ω)))]
+    
+    def addSample(self, Length: int):
+        fArray = Maps.randFloats(Length)
+        ω = [Maps.float2HT(fArray[i]) for i in range(Length)]
+        self.SampleSpace.append(ω)
+        self.RandVariables.append(self.RandVariable(ω))
+        
+    def addSamples(self, sizes):
+        events = [self.addSample(size) for size in sizes]
+        for event in events:
+            if not event == None:
+                self.SampleSpace.append(event)
